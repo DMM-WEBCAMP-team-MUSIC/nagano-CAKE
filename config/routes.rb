99b9aa51aa5a013068a25224b126db34aaa7af23
Sa110-5325 <sa110.35325@gmail.com>
2021-01-19 08:27:26 +0000

    商品新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :items
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/show'
-    get 'items/new'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :customers do
-    get 'ordered_items/index'
-    get 'ordered_items/show'
-    get 'ordered_items/new'
-    get 'ordered_items/finish'
-  end
-  namespace :customers do
-    get 'cart_items/index'
-  end
-  namespace :customers do
-    get 'shippings/index'
-    get 'shippings/edit'
-  end
-  namespace :customers do
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :customers do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :customers do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :customers do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  
-=======
-  
+
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
     resources :items, only: [:index, :show, :new, :create, :edit, :update]
@@ -76,7 +23,6 @@ Rails.application.routes.draw do
     post "ordered_items/confirm"
     get "ordered_items/finish"
   end
->>>>>>> 43941bc42dfa95af1e6d2d3814dd464b454273b4
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210117130658_create_items.rb
+++ db/migrate/20210117130658_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.integer :genre_id
       t.string :name
-      t.string :image
+      t.string :image_id
       t.text :info
       t.integer :price
       t.boolean :status
