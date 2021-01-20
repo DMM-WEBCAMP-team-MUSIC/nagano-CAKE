372206ea35fea1e7acc0e4e6b62ef4d7dbcd38b3
okadareo <okadareo0614@gmail.com>
2021-01-20 01:20:23 +0000

    devise新規投稿画面完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    registrations: "admins/registration"
+  }
+  devise_for :customers, controllers: {
+    sessions: "customers/sessions",
+    passwords: "customers/passwords",
+    registrations: "customers/registrations"
+  }
+
+  resources :customers
 end
