18fb450dbbd9201b79efc8b0f0af6fa1d303053e
Sa110-5325 <sa110.35325@gmail.com>
2021-01-20 07:28:35 +0000

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,8 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :ordered_items
   belongs_to :genre
+  
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true
 end
