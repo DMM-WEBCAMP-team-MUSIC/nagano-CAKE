d5cf6df18ad8054416d2db0c1b9dd86b389568aa
Sa110-5325 <sa110.35325@gmail.com>
2021-01-21 02:12:28 +0000

    admin完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,13 +6,13 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :postal_code, presence: true
-  validates :adress, presence: true
+  validates :address, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
 end
--- db/migrate/20210118053412_create_orders.rb
+++ db/migrate/20210118053412_create_orders.rb
@@ -7,8 +7,8 @@ class CreateOrders < ActiveRecord::Migration[5.2]
       t.string :postal_code
       t.string :address
       t.integer :total_fee
-      t.integer :payment, null: false,default: '0'
-      t.integer :status, null: false,default: '0'
+      t.integer :payment, null: false,default: 0
+      t.integer :status, null: false,default: 0
 
       t.timestamps
     end
