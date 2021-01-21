3a342091552083dee71d46daac9b3719d51d8280
OTAKE-Soya <oyassun@gmail.com>
2021-01-21 03:17:31 +0000

    商品詳細ページのカートに追加ボタン実装（同種類商品の追加については追って）

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -3,6 +3,13 @@ class Customers::CartItemsController < ApplicationController
   end
   
   def create
+    item = CartItem.new(cart_item_params)
+    item.customer_id = current_customer.id
+    if item.save
+      redirect_to cart_items_path
+    else
+      
+    end
   end
   
   def all_destroy
@@ -12,5 +19,10 @@ class Customers::CartItemsController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity, :item_id)
   end
 end
