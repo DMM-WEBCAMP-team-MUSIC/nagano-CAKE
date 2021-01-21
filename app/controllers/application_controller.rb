2193fd111e02215d4c06a283f579a191806f3519
EC2 Default User <ec2-user@ip-172-31-54-34.ec2.internal>
2021-01-21 10:38:20 +0000

    notice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :phone_number,:address])
   end
+  
+  private
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 end
