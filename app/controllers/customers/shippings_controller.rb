0a294a37fb43286ff6fdf86e32e89c44203815ef
nami-s <na.s1222.10feettt@gmail.com>
2021-01-21 10:30:20 +0000

    [finish]配送先関連

--- app/controllers/customers/shippings_controller.rb
+++ app/controllers/customers/shippings_controller.rb
@@ -1,6 +1,7 @@
 class Customers::ShippingsController < ApplicationController
+  
   def index
-    @shippings = Shipping.all
+    @shippings = current_customer.shippings
     @shipping = Shipping.new
   end
 
@@ -9,16 +10,35 @@ class Customers::ShippingsController < ApplicationController
   end
   
   def create
+    @shippings = current_customer.shippings
     @shipping = Shipping.new(shipping_params)
+    @shipping.customer_id = current_customer.id
     if @shipping.save
       redirect_to shippings_path
+    else
+      render "index"
     end
   end
   
   def update
+    @shipping = Shipping.find(params[:id])
+    if @shipping.update(shipping_params)
+      redirect_to shippings_path
+    else
+      render "edit"
+    end
   end
   
   def destroy
+    @shipping = Shipping.find(params[:id]) 
+    @shipping.destroy
+    redirect_to shippings_path
+  end
+  
+  private
+  
+  def shipping_params
+    params.require(:shipping).permit(:postal_code, :address, :name)
   end
   
 end
