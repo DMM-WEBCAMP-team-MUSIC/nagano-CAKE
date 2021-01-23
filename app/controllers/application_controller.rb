6f8fdb5b7b338ae7c0bf01ba2f58c792edc0d038
okadareo <okadareo0614@gmail.com>
2021-01-23 07:32:04 +0000

    マイページ編集途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :phone_number,:address])
-
+　　devise_parameter_sanitizer.permit(:account_update, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :phone_number, :address, :email])
   end
   
   private
--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -5,14 +5,25 @@ class Customers::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customers_path
+    end
   end
 
   def withdraw
   end
 
   def unsubscribe
+  end
+
+  private
+
+  def customer_params
+   params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email)
   end
 end
