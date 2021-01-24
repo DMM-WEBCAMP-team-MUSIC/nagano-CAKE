4c99f54ed9a3490d619b0e117951c44b96161490
okadareo <okadareo0614@gmail.com>
2021-01-24 10:15:17 +0000

    kaminari機能追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,11 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @oders = Oder.page(params[:page]).per(10)
   end
 
   def show
   end
-  
+
   def update
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,25 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    items_path
+    case resource
+    when Customer
+      items_path
+    when Admin
+      admin_customers_path
+    end
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    case resource
+    when :customer
+      root_path
+    when :admin
+      new_admin_session_path
+    end
   end
-
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
   end
-
-  private
-
-
-
-
-
-
-
-
-
-
 end
