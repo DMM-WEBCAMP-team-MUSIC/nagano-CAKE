fb588cab024bdb6fdf00dc357a90364f835ea1f1
okadareo <okadareo0614@gmail.com>
2021-01-25 09:08:07 +0000

    admin注文詳細途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,12 +1,31 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @orders = Order.page(params[:page]).per(10)
+    @orders = Order.all.page(params[:page]).per(10)
   end
 
   def show
+    @order = Order.find(params[:id])
+    @ordered_items = @order.ordered_items
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      if @order.status == "入金確認"
+        @order.ordered_items.each do |ordered_item|
+          ordered_item.update(status: "製作待ち")
+        end
+      end
+      redirect_to admin_order_path(@order)
+    else
+      render "show"
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 
 end
