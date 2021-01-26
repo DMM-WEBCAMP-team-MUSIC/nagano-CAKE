94fe26ac69239caf7e68c9de84e15ac3fd02fcf5
OTAKE-Soya <oyassun@gmail.com>
2021-01-26 07:25:12 +0000

    できた。論理削除後の会員のログインをはじく機能が

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,6 +16,10 @@ class Customer < ApplicationRecord
   validates :phone_number, presence: true
   validates :email, presence: true
   
+  def active_for_authentication?
+    super && self.status
+  end
+  
   def total_payment
     total = 0 #totalは最初は0円
     self.cart_items.each do |cart_item| #全商品繰り返し
