83b5cd32a0dbbe0b4fba19d6e3110cc363cc2528
okadareo <okadareo0614@gmail.com>
2021-01-26 10:58:52 +0000

    ログイン後の遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Customer
-      items_path
+      customers_path
     when Admin
       admin_customers_path
     end
